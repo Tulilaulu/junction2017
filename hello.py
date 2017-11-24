6ca38832be5177b10104312b9f737331c99a9c9c
Tulilaulu Aurora <tulilaa1@niksula.hut.fi>
2017-11-24 20:44:42 +0200

    ljdsflk

--- hello.py
+++ hello.py
@@ -1,7 +1,17 @@
 from bottle import route, run, template
+from urllib2 import Request, urlopen, URLError
+import requests
 
-@route('/hello/<name>')
-def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+@route('/data/')
+def index():
+    data = {"password": "ratkaisutalkootapahtuma"}
+    r = requests.post('https://fortum.hackjunction.com/api/locations/'+'3', data=data).text
+    try:
+#        response = urlopen(r)
+#        a = response.read()
+        print r
+        return template('<pre>{{r}}</pre>!')
+    except URLError, e:
+        print 'No kittez. Got an error code:', e    
 
 run(host='localhost', port=8080)
