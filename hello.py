67ea593dbff04cd445f8d08d32e049091c3669f1
Tulilaulu Aurora <tulilaa1@niksula.hut.fi>
2017-11-24 21:46:54 +0200

    GET stuff

--- hello.py
+++ hello.py
@@ -1,13 +1,20 @@
 from bottle import route, run, template
 from urllib2 import Request, urlopen, URLError
 import requests
+import json
 
 @route('/data/')
 def index():
     data = {"password": "ratkaisutalkootapahtuma"}
-    r = requests.post('https://fortum.hackjunction.com/api/locations/', json=data).text
+    r = json.loads(requests.post('https://fortum.hackjunction.com/api/locations/', json=data).text)
+    print r
     try:
-        print r
+        for item in r:
+            print "Getting "+str(item['location'])
+            i = requests.post('https://fortum.hackjunction.com/api/locations/'+str(item['location']), json=data).text
+            r.append(i)
+            if (item['location'] > 3):
+                break
         return template('<pre>{{r}}</pre>!', r=r)
     except URLError, e:
         print 'No kittez. Got an error code:', e
