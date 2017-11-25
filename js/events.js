ec0adbe3cf9c63d454f14215ec455acfa03d3d12
Paul Saikko <paul.saikko@cs.helsinki.fi>
2017-11-25 21:42:50 +0200

    bind arrow keys

--- js/events.js
+++ js/events.js
@@ -21,22 +21,30 @@ $(function() {
   window.dateIndex = 8;
 
   $('#debug_prev').click(function(){
-    window.dateIndex--;
-    if (window.dateIndex < 8) {
-      window.dateIndex = 8;
-    } else {
-      window.refresh_plot();
-    }
+    window.prevDay();
   });
 
   $('#debug_next').click(function(){
+    window.nextDay();
+  });
+
+  window.nextDay = function() {
     window.dateIndex++;
     if (window.maxIndex < window.dateIndex) {
       window.dateIndex = window.maxIndex;
     } else {
       window.refresh_plot();
     }
-  });
+  }
+
+  window.prevDay = function() {
+    window.dateIndex--;
+    if (window.dateIndex < 8) {
+      window.dateIndex = 8;
+    } else {
+      window.refresh_plot();
+    }
+  }
 
   window.onBetter = function() {
     console.log("Better!");
@@ -45,5 +53,20 @@ $(function() {
   window.onWorse = function() {
     console.log("Worse!");
   }
+
+  $(document).keydown(function(e) {
+    switch(e.which) {
+        case 39: // left
+          window.nextDay();
+        break;
+
+        case 37: // right
+          window.prevDay();
+        break;
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+  });
 
 });
